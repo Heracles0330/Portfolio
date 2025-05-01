1aaa609797e9a2c2553e4148d620b192ed00415d
Heracles0330 <heracles0330@Gmail.com>
2025-05-01 11:01:58 +0900

    main branch

---
+++ utils/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'heracles0330@gmail.com',
+    phone: '+573171426409',
+    address: 'Los Angeles, California ',
+    github: 'https://github.com/Heracles0330',
+    devUsername: "Heracles0330"
+}
---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "2020 - Present",
+    institution: "University of California, Los Angeles (UCLA)",
+  },
+  {
+    id: 2,
+    title: "Higher Secondary Certificate",
+    duration: "2018 - 2020",
+    institution: "Harvard-Westlake School, Los Angeles",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    duration: "2008 - 2018",
+    institution: "Loyola High School, Los Angeles",
+  }
+]
